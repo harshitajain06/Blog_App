3f4d2aaca2b292872bed62cda03d9860e90be1f4
harshitajain06 <harshitaforever06@gmail.com>
2023-04-08 04:51:39 +0530

    correct linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  def index;
-  @posts = Post.all
-  @user = User.find(params[:user_id])
-end
+  def index
+    @posts = Post.all
+    @user = User.find(params[:user_id])
+  end
 
   def show
     @post = Post.find(params[:id])
