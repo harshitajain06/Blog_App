b1c7039ceb0b95ef92a83bd61f121c548330d59b
harshitajain06 <harshitaforever06@gmail.com>
2023-04-08 20:37:28 +0530

    add changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,11 @@ class LikesController < ApplicationController
     @user = current_user
     @like = Like.new(author: @user, post: @post)
     @like.save
-    
+
     if @like.save
-         redirect_to user_post_path(@user, @post) 
-      else
-         render :new 
-      end
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new
+    end
   end
 end
