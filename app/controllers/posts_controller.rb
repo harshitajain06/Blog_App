c71d0d618dc9b6f2ae1315055de4cc4ee496cd1a
harshitajain06 <harshitaforever06@gmail.com>
2023-04-08 19:15:20 +0530

    add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @user = User.find(params[:user_id])
-    @comments = Comment.all
+    @comments = @post.comments
+    @likes = @post.likes
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = Post.new(author: @user, title: params[:post][:title], text: params[:post][:text])
+    @post.save
+    redirect_to user_path(@user)
   end
 end
