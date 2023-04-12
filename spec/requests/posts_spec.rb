3f357f4ea19f17f42d0bd34f0a86ed84e5a3d312
harshitajain06 <harshitaforever06@gmail.com>
2023-04-12 21:21:33 +0530

    add feature tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,35 +2,45 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before(:example) do
+      @user = User.create(name: 'Dev', photo: 'myPhoto', bio: 'myBio', post_counter: 0)
+      @post = Post.create(title: 'myTitle', text: 'Hello', author: @user, comments_counter: 0, likes_counter: 0)
+      get user_posts_path(@user)
+    end
     it 'returns a successful response' do
-      get '/users/:user_id/posts'
+      get user_posts_path(@user)
       expect(response).to be_successful
     end
 
     it 'renders the correct template' do
-      get '/users/:user_id/posts'
+      get user_posts_path(@user)
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/:user_id/posts'
-      expect(response.body).to include('Here is a list of posts for a given user')
+      get user_posts_path(@user)
+      expect(response.body).to include('myTitle')
     end
   end
   describe 'GET /show' do
+    before(:example) do
+      @user = User.create(name: 'Dev', photo: 'myPhoto', bio: 'myBio', post_counter: 0)
+      @post = Post.create(title: 'myTitle', text: 'Hello', author: @user, comments_counter: 0, likes_counter: 0)
+      get user_post_path(@user, @post)
+    end
     it 'returns a successful response' do
-      get '/users/:user_id/posts/:post_id/'
+      get user_post_path(@user, @post)
       expect(response).to be_successful
     end
 
     it 'renders the correct template' do
-      get '/users/:user_id/posts/:post_id/'
+      get user_post_path(@user, @post)
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/:user_id/posts/:post_id/'
-      expect(response.body).to include('Selected post for a given user')
+      get user_post_path(@user, @post)
+      expect(response.body).to include('Create new comment')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before(:example) do
+      @user = User.create(name: 'Dev', photo: 'myPhoto', bio: 'myBio', post_counter: 0)
+    end
+  
     it 'returns a successful response' do
       get '/users'
       expect(response).to be_successful
@@ -14,24 +18,29 @@ RSpec.describe 'Users', type: :request do
 
     it 'includes correct placeholder text in the response body' do
       get '/users'
-      expect(response.body).to include('All the users')
+      expect(response.body).to include('Dev')
     end
   end
 
   describe 'GET /show' do
+    before(:example) do
+      @user = User.create(name: 'Dev', photo: 'myPhoto', bio: 'myBio', post_counter: 0)
+      get user_path(@user)
+    end
+  
     it 'returns a successful response' do
-      get '/users/:id/'
+      get user_path(@user)
       expect(response).to be_successful
     end
 
     it 'renders the correct template' do
-      get '/users/:id/'
+      get user_path(@user)
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/:id/'
-      expect(response.body).to include('Selected user')
+      get user_path(@user)
+      expect(response.body).to include('Create post')
     end
   end
 end
